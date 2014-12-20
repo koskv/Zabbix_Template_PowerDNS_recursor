26a67dfce3edf2ee3d2aa848340901d9e010e633
kometchtech <kometch@gmail.com>
2014-12-20 22:22:53 +0900

    pdns-recursor Running check

--- zbx_send_pdns_rec.sh
+++ zbx_send_pdns_rec.sh
@@ -8,30 +8,35 @@ HOSTNAME=`hostname`" "
 PDNS_REC_CTL="/usr/bin/rec_control"
 TMP_FILE="/tmp/pdns_rec.txt"
 
-# ping Alive
-ping -c 1 ${ZBX_SERVER} > /dev/null 2>&1
-
+# Running chech pdns-recursor
+`${PDNS_REC_CTL} get-all`
 if [ $? -eq 0 ]; then
 
-	# PowerDNS recursor stats
-
-	# file delete
-	if [ -f ${TMP_FILE} ]; then
-		rm ${TMP_FILE}
-	fi
-
-	array=()
-
-	# rec_control get-all
-	array=(`${PDNS_REC_CTL} get-all | cut -f1`)
-
-	for (( i = 0; i < ${#array[@]}; i++ ))
-	do
-		# rec_control valiable
-		echo ${HOSTNAME} ${array[i]} ${UNIXTIME} `${PDNS_REC_CTL} get ${array[i]}` >> ${TMP_FILE}
-	done
+	# ping Alive
+	ping -c 1 ${ZBX_SERVER} > /dev/null 2>&1
 	
-	# Zabbix send data
-	${ZBX_SENDER} -z ${ZBX_SERVER} -p ${ZBX_SERVER_PORT} -T -i ${TMP_FILE} > /dev/null
+	if [ $? -eq 0 ]; then
+	
+		# PowerDNS recursor stats
+	
+		# file delete
+		if [ -f ${TMP_FILE} ]; then
+			rm ${TMP_FILE}
+		fi
+	
+		array=()
+	
+		# rec_control get-all
+		array=(`${PDNS_REC_CTL} get-all | cut -f1`)
+	
+		for (( i = 0; i < ${#array[@]}; i++ ))
+		do
+			# rec_control valiable
+			echo ${HOSTNAME} ${array[i]} ${UNIXTIME} `${PDNS_REC_CTL} get ${array[i]}` >> ${TMP_FILE}
+		done
+		
+		# Zabbix send data
+		${ZBX_SENDER} -z ${ZBX_SERVER} -p ${ZBX_SERVER_PORT} -T -i ${TMP_FILE} > /dev/null
 
+	fi
 fi
